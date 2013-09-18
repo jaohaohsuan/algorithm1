e410d7d27cbdcedc3214867562f95ba270b8eef4
henry <jaohaohsuan@gmail.com>
2013-09-18 15:35:10 +0800

    initial

---
+++ ClassLibrary1.Tests/MapBuilderTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ClassLibrary1.Tests
+{
+    [TestFixture]
+    public class MapBuilderTest
+    {
+        [Test]
+        public void LevelTest()
+        {
+            var rules = new[]
+                {
+                    new Rule { One = "A", Two = "B" },
+                    new Rule { One = "A", Two = "C" },
+                };
+            var map = MapBuilder.Build(rules);
+
+            Assert.AreEqual(map["A"].Level, 1);
+        }
+    }
+}
---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Rule
+    {
+        public string One { get; set; }
+        public string Two { get; set; }
+    }
+
+    public class MapBuilder
+    {
+        public static  IDictionary<string,Ball> Build(IEnumerable<Rule> rules)
+        {
+            var uniqStoreage = new ConcurrentDictionary<string, Ball>();
+            foreach (var rule in rules)
+            {
+                Ball one, two;
+                uniqStoreage.TryAdd(rule.One, new Ball(rule.One));
+                one = uniqStoreage[rule.One];
+                uniqStoreage.TryAdd(rule.Two, new Ball(rule.Two));
+                two = uniqStoreage[rule.Two];
+
+                one.Balls.Add(two);
+            }
+
+            return uniqStoreage;
+        }
+    }
+
+    public class Ball
+    {
+        public Ball(string name)
+        {
+            Balls = new List<Ball>();
+        }
+
+        public List<Ball> Balls { get; set; }
+        public string Name { get; private set; }
+
+        private int? _level;
+        public int Level
+        {
+            get
+            {
+                if (_level == null)
+                {
+                    if (Balls.Any())
+                        _level = Balls.Max(o => o.Level) + 1;
+                    else
+                        _level = 0;
+
+                }
+                return _level.Value;
+            }
+        }
+    }
+}
